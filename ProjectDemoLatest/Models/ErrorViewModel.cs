9b652b38966aba9cd946987d9d3c74f3d4b571f5
nirajuprety <nerajupreti@gmail.com>
2022-11-27 15:46:19 +0545

    inital commit

---
+++ ProjectDemoLatest/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProjectDemoLatest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
